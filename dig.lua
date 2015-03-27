8368836bb1b9e8857ffc885ab03e39631006f329
Deliquescence <misterbeez01@gmail.com>
2015-03-27 19:18:41 -0400

    add dig again

--- dig.lua
+++ dig.lua
@@ -1,6 +1,11 @@
 local args = { ... }
 
 if(#args > 0) then
-  if (args[0] == "up") then
-    turtle.digUp()
-   else if (
+    if (args[1] == "up") then
+        turtle.digUp()
+    elseif (args[1] == "down") then
+        turtle.digDown()
+    end
+else
+    turtle.dig()
+end
